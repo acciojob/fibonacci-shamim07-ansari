765c5ddb9edf38a7b34877609335d4d4f8396ad5
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-14 20:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
-}
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	for(let i=0; i<num; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
+} 
 
 module.exports = fibonacci;
