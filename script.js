d5da29ecf3fbdd2d33db7eef830d78f3a1f0833d
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-14 20:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function fibonacci(num) {
-	int a = 0;
-	int b = 1;
-	int c = 0;
-	for(let i=0; i<num; i++) {
+	if(num === 1){
+		return 0;
+	}
+	if(num === 2){
+		return 1;
+	}
+	let a = 0;
+	let b = 1;
+	let c = 0;
+	for(let i=3; i<=num; i++){
 		c = a + b;
 		a = b;
 		b = c;
